a6fda353a771732adbc0ce403573eff4e7d50ce2
jeevananthan27 <106263059+jeevananthan27@users.noreply.github.com>
2023-03-06 21:10:28 +0530

    Add files via upload

---
+++ bitwise oper.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void main()
+
+{
+   
+ //bitwise 
+ 
+   int a;
+  
+   int b;
+ 
+   printf("enter two value:\n");
+ 
+ 
+   scanf("%d%d",&a,&b);
+  
+
+   printf("%d",a&b);
+
+  
+  if(a|b)
+  
+  {
+       
+
+ printf("or operator");
+   
+ 
+    }
+
+
+}
